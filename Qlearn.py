50485476321b3e2a3edbec20a49fe07fae0f9577
Dominic Inauen <dominic.inauen@bluewin.ch>
2023-01-28 23:24:49 +0100

    Added code to allow learning on rotation of considered state

--- Qlearn.py
+++ Qlearn.py
@@ -89,7 +89,7 @@ class Agent:
             index = np.argmax(self.Q[self.state_to_index_(self.env.return_state())])
             return list(self.env.actions.keys())[index]
         
-    def train(self, episodes, max_eps, min_eps, learning_rate, decay_rate, gamma, max_steps):
+    def train(self, episodes, max_eps=1.0, min_eps=0.05, learning_rate=0.7, decay_rate=0.0005, gamma=0.95, max_steps=10):
         '''trains the model, modifying the Q-table.'''
         for episode in range(episodes):
             self.env = Environment()
@@ -101,13 +101,32 @@ class Agent:
                 state = self.env.return_state()
                 new_state, reward = self.env.perform_action(action_key)
 
-                action_index = np.argwhere(np.array(list(self.env.actions.keys())) == action_key)[0][0]
-                self.Q[self.state_to_index_(state)][action_index] = self.Q[self.state_to_index_(state)][action_index] + learning_rate * (reward + gamma * np.max(self.Q[self.state_to_index_(new_state)]) - self.Q[self.state_to_index_(state)][action_index])
+                #edit Q-table for every valid rotation of state and action
+                action_tuples = [(state, action_key, new_state)]
+                tmpstate = state
+                tmpactionkey = action_key
+                tmpnewstate = new_state
+                for i in range(3):
+                    tmpstate = self.env.rotate_state_(tmpstate)
+                    tmpactionkey = list(self.env.actions.keys())[list(self.env.actions.values()).index(list(self.env.rotate_state_(self.env.actions[tmpactionkey])))]
+                    tmpnewstate = self.env.rotate_state_(tmpnewstate)
+                    not_present = True
+                    for entry in action_tuples:
+                        if np.all(entry[0] == tmpstate):
+                            not_present = False
+                            break
+                    if not_present == True:
+                        action_tuples.append((tmpstate, tmpactionkey, tmpnewstate))
+
+                for (state, action_key, new_state) in action_tuples:
+                    action_index = np.argwhere(np.array(list(self.env.actions.keys())) == action_key)[0][0]
+                    stateindex = self.state_to_index_(state)
+                    self.Q[stateindex][action_index] = self.Q[stateindex][action_index] + learning_rate * (reward + gamma * np.max(self.Q[self.state_to_index_(new_state)]) - self.Q[stateindex][action_index])
 
                 if reward != 0:
                     break
 
-            if episode % int(episodes/10) == 0:
+            if (episode+1) % int(episodes/20) == 0:
                 print(int(episode/episodes * 100), "% done...")
 
     def solve(self, max_steps = 10):
